8dea2853a11bc37ce95fbdb221cd3b3f7ae9d612
mattatttaq <mattatttaq@gmail.com>
2018-09-29 21:24:21 -0400

    Updated scripts
    * added offset on hash click

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+        e.preventDefault();
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate( {
+            'scrollTop': $target.offset().top-90
+        });
+        } );
+});
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,1 @@
-
+$(document).ready(function(){$('a[href^="#"]').bind("click.smoothscroll",function(o){o.preventDefault();var t=this.hash,e=$(t);$("html, body").stop().animate({scrollTop:e.offset().top-90})})});
